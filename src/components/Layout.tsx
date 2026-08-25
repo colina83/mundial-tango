1d4fd5ec6b47496986524cd214f489ca2538b906
Francisco Colina <franco.collina@gmail.com>
2026-08-25 10:06:46 -0500

    Remove "Mundial de Tango" text below PULSO wordmark in header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -24,7 +24,6 @@ export function Layout() {
           <NavLink to="/" className="brand">
             <span className="brand-text">
               <span className="pulso-wordmark">{t("brand")}</span>
-              <strong>{t("appName")}</strong>
               <em>{t("tagline")}</em>
             </span>
           </NavLink>
